7bb32f09da344306cdfea4f66e4aaf00385b4df9
James Rosado <jamesros@boldgrid.com>
2022-12-21 15:06:29 -0500

    change comment on get_post_status

--- includes/class-boldgrid-inspirations-purchase-for-publish.php
+++ includes/class-boldgrid-inspirations-purchase-for-publish.php
@@ -988,7 +988,7 @@ class Boldgrid_Inspirations_Purchase_For_Publish extends Boldgrid_Inspirations {
 	 *
 	 * @since 1.1.4
 	 *
-	 * @return array an array used after in IN statement, example: ( 'draft', 'publish' ).
+	 * @return array An array of post status., example: ( 'draft', 'publish' ).
 	 */
 	public function get_post_status() {
 		$post_status = array( 'draft', 'publish' );
