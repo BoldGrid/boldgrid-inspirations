2fee52c6ef8ea10e5cd87ba55aed710edbacec32
James Rosado <jamesros@boldgrid.com>
2023-09-14 13:18:48 -0400

    add card data generation methods

--- includes/class-boldgrid-inspirations-onboarding-tasks.php
+++ includes/class-boldgrid-inspirations-onboarding-tasks.php
@@ -54,7 +54,6 @@ class Boldgrid_Inspirations_Onboarding_Tasks {
 	 *
 	 * @param array $install_options The install options.
 	 */
-
 	public function create_tasks( $install_options ) {
 		$available_tasks = $this->configs['available_onboarding_tasks'];
 		$active_tasks    = array();
@@ -81,5 +80,81 @@ class Boldgrid_Inspirations_Onboarding_Tasks {
 
 		// Save the active tasks.
 		update_option( $this->option_name, $active_tasks );
+	}
+
+	/**
+	 * Get Cards Data
+	 *
+	 * @since SINCEVERSION
+	 *
+	 * @return array An array of cards and their tasks.
+	 */
+	public function get_cards_data() {
+		$available_cards = $this->configs['available_onboarding_cards'];
+
+		$active_cards = array();
+
+		foreach ( $available_cards as $card ) {
+			$tasks         = $this->get_tasks_for_card( $card['id'] );
+			$card['tasks'] = $tasks;
+			if ( ! empty( $tasks ) ) {
+				$active_cards[] = $card;
+			}
+		}
+
+		return $active_cards;
+	}
+
+	/**
+	 * Get Tasks for card
+	 *
+	 * @since SINCEVERSION
+	 *
+	 * @param string $card_id The card ID.
+	 *
+	 * @return array The tasks for the card.
+	 */
+	public function get_tasks_for_card( $card_id ) {
+		$tasks = get_option( $this->option_name );
+
+		if ( ! is_array( $tasks ) ) {
+			return array();
+		}
+
+		$tasks_for_card = array();
+
+		foreach ( $tasks as $task ) {
+			if ( $task['card_id'] === $card_id ) {
+				$tasks_for_card[] = $task;
+			}
+		}
+
+		return $tasks_for_card;
+	}
+
+	/**
+	 * Has Legacy Headers
+	 *
+	 * @since SINCEVERSION
+	 *
+	 * @param array $install_options
+	 *
+	 * @return boolean True if the theme has legacy headers, false otherwise.
+	 */
+	public function has_legacy_headers( $install_options ) {
+		return isset( $install_options['theme_has_cph'] ) && true === $install_options['theme_has_cph'] ? false : true;
+	}
+
+	/**
+	 * Has Custom Headers
+	 *
+	 * @since SINCEVERSION
+	 *
+	 * @param array $install_options
+	 *
+	 * @return boolean True if the theme has custom headers, false otherwise.
+	 */
+	public function has_custom_headers( $install_options ) {
+		return isset( $install_options['theme_has_cph'] ) && true === $install_options['theme_has_cph'] ? true : false;
 	}
 }
